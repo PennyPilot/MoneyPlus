a77ea3df2d751570c7cc7a43aa5cff6b78024f55
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2026-01-13 21:59:52 +0200

    refactor: add a fixed width for tabs

--- lib/design_system/component/nav_bar.dart
+++ lib/design_system/component/nav_bar.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:moneyplus/design_system/theme/money_extension_context.dart';
 import 'package:moneyplus/utils/Assets.dart';
 
 enum NavBarTab {
   home(Assets.icHomePrimary, Assets.icHomeGray, 'Home'),
-  transaction(Assets.icTransactionPrimary, Assets.icTransactionGray, 'Transaction'),
+  transaction(
+    Assets.icTransactionPrimary,
+    Assets.icTransactionGray,
+    'Transaction',
+  ),
   statistics(Assets.icStatisticsPrimary, Assets.icStatisticsGray, 'Statistics'),
   account(Assets.icAccountPrimary, Assets.icAccountGray, 'Account');
 
   final String assetSelected;
-  final String assetNotSelected;
+  final String assetUnselected;
   final String title;
 
-  const NavBarTab(this.assetSelected, this.assetNotSelected, this.title);
+  const NavBarTab(this.assetSelected, this.assetUnselected, this.title);
 }
 
 class NavBar extends StatelessWidget {
   final NavBarTab selectedTab;
   final void Function(NavBarTab) onTabSelected;
 
-  const NavBar({super.key, required this.selectedTab, required this.onTabSelected});
+  const NavBar({
+    super.key,
+    required this.selectedTab,
+    required this.onTabSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -37,39 +46,50 @@ class NavBar extends StatelessWidget {
           final isSelected = tab == selectedTab;
 
           return GestureDetector(
-            onTap: () { onTabSelected(selectedTab); },
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SvgPicture.asset(
-                  isSelected? tab.assetSelected : tab.assetNotSelected,
-                  width: 24,
-                  height: 24,
-                ),
-                if(!isSelected) SizedBox(height: 8,),
-                if (isSelected) ...[
-                  Text(tab.title, style: typography.label.small.copyWith(color: colors.primary)),
-                  Container(
-                    width: 32,
-                    height: 4,
-                    padding: EdgeInsetsGeometry.directional(top: 11),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: colors.primary.withOpacity(0.2),
-                          blurRadius: 16,
-                          spreadRadius: 6
-                        ),
-                      ],
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(4),
-                        topRight: Radius.circular(4),
-                      ),
-                      color: colors.primary,
-                    ),
+            onTap: () {
+              onTabSelected(selectedTab);
+            },
+            child: SizedBox(
+              width: 82,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SvgPicture.asset(
+                    isSelected ? tab.assetSelected : tab.assetUnselected,
+                    width: 24,
+                    height: 24,
                   ),
+                  if (!isSelected) SizedBox(height: 8),
+                  if (isSelected) ...[
+                    Text(
+                      tab.title,
+                      style: typography.label.small.copyWith(
+                        color: colors.primary,
+                      ),
+                    ),
+                    Container(
+                      width: 32,
+                      height: 4,
+                      padding: EdgeInsetsGeometry.directional(top: 11),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: colors.primary.withOpacity(0.2),
+                            blurRadius: 16,
+                            spreadRadius: 2,
+                            offset: Offset.fromDirection(0, -4),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(4),
+                          topRight: Radius.circular(4),
+                        ),
+                        color: colors.primary,
+                      ),
+                    ),
+                  ],
                 ],
-              ],
+              ),
             ),
           );
         }).toList(),
