a3ef341453050256747016c5b2aadb0a3452fdc6
mohamedshemees <mohamedessampd@gmail.com>
2026-01-14 15:39:06 +0200

    feat: add SectionHeader component

---
+++ lib/design_system/assets/money_images.dart
@@ -0,0 +1,5 @@
+
+class MoneyAssets {
+  MoneyAssets._();
+  static const String icArrowRight = "assets/images/ic_arrow_right.svg";
+}
---
+++ lib/design_system/widget/section_header.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../assets/money_images.dart';
+import '../theme/money_extension_context.dart';
+
+class SectionHeader extends StatelessWidget {
+  final String? title;
+  final Widget? leadingContent;
+  final Widget? trailingContent;
+  final VoidCallback? onClickTrailingContent;
+
+  const SectionHeader({
+    super.key,
+    this.title,
+    this.leadingContent,
+    this.trailingContent,
+    this.onClickTrailingContent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        leadingContent ?? _buildDefaultLeadingContent(context),
+        GestureDetector(
+          onTap: () => {onClickTrailingContent?.call()},
+          child: trailingContent ?? _buildDefaultTrailingContent(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDefaultTrailingContent() {
+    return SvgPicture.asset(MoneyAssets.icArrowRight, height: 20, width: 20);
+  }
+
+  Widget _buildDefaultLeadingContent(BuildContext context) {
+    final colors = context.colors;
+    final typography = context.typography;
+    return Text(
+      title ?? "",
+      style: typography.title.small.copyWith(color: colors.title),
+    );
+  }
+}
