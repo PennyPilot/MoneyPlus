f0db286034b26d326211a55d1441a34422c11842
mohamedshemees <mohamedessampd@gmail.com>
2026-01-16 01:25:01 +0200

    core: initialize Supabase

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../data/repository/authentication_repository.dart';
 import '../domain/repository/authentication_repository.dart';
@@ -11,7 +13,14 @@ Future<void> initDI() async {
   getIt.registerLazySingleton<AuthenticationRepository>(
     () => AuthenticationRepositoryImp(),
   );
-
+  await dotenv.load(fileName: "secrets.env");
+  getIt.registerSingleton<Supabase>(
+    await Supabase.initialize(
+      url: dotenv.env['SUPA_BASE_URL'] ?? "",
+      anonKey: dotenv.env['SUPA_API_KEY'] ?? "",
+      debug: true,
+    ),
+  );
   getIt.registerFactory<LoginCubit>(() => LoginCubit());
   getIt.registerFactory<HomeCubit>(() => HomeCubit());
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:moneyplus/di/injection.dart';
 import 'package:moneyplus/money_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    debugPrint('${record.level.name}: ${record.time}: ${record.message}');
+    if (record.error != null) {
+      debugPrint('${record.error}');
+    }
+  });
+  await initDI();
+
   runApp(const MoneyApp());
 }
