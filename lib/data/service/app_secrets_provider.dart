a7e255a5db99dd8299c9bdd0f616dc8fbbb0c1e7
mohamedshemees <mohamedessampd@gmail.com>
2026-01-16 23:06:31 +0200

    refactor: extract  Supabase and secrets initialization and provide a getterFunction for it

---
+++ lib/data/service/app_secrets_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import '../../core/app_constants.dart';
+
+class AppSecretsProvider {
+  DotEnv? _dotenv;
+
+  Future<DotEnv> getEnvVariables() async {
+    if (_dotenv != null) return _dotenv!;
+    _dotenv = DotEnv();
+    await _dotenv!.load(fileName: AppConstants.secretsEnvFile);
+    return _dotenv!;
+  }
+}
--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,25 +1,23 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../data/repository/authentication_repository.dart';
+import '../data/service/app_secrets_provider.dart';
+import '../data/service/supabase_service.dart';
 import '../domain/repository/authentication_repository.dart';
 import '../presentation/home/cubit/home_cubit.dart';
 import '../presentation/login/cubit/login_cubit.dart';
 
 final getIt = GetIt.instance;
 
-Future<void> initDI() async {
-  getIt.registerLazySingleton<AuthenticationRepository>(
-    () => AuthenticationRepositoryImp(),
+void initDI() {
+  getIt.registerLazySingleton<AppSecretsProvider>(() => AppSecretsProvider());
+  getIt.registerLazySingleton<SupabaseService>(
+    () => SupabaseService(appSecretsProvider: getIt<AppSecretsProvider>()),
   );
-  await dotenv.load(fileName: "secrets.env");
-  getIt.registerSingleton<Supabase>(
-    await Supabase.initialize(
-      url: dotenv.env['SUPA_BASE_URL'] ?? "",
-      anonKey: dotenv.env['SUPA_API_KEY'] ?? "",
-      debug: kDebugMode,
+  getIt.registerLazySingleton<AuthenticationRepository>(
+    () => AuthenticationRepositoryImpl(
+      supabaseService: getIt<SupabaseService>(),
+      appSecrets: getIt<AppSecretsProvider>(),
     ),
   );
 
