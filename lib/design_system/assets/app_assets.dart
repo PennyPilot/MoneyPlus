d53159102afcde037b090ae9153f348a470e1fe2
nada <nadamansoursamir@gmail.com>
2026-01-17 01:38:04 +0200

    feat: add search, calendar, and money icons and update AppAssets

--- lib/design_system/assets/app_assets.dart
+++ lib/design_system/assets/app_assets.dart
@@ -1,13 +1,17 @@
 class AppAssets {
   AppAssets._();
 
-  static const _icons = 'assets/icons';
+  static const _icons = 'assets/icons/';
 
-  static const icArrowDown = '$_icons/ic_arrow_down.svg';
-  static const icArrowUp = '$_icons/ic_arrow_up.svg';
-  static const icArrowRight ='$_icons/ic_arrow_right.svg';
+  static const icArrowDown = '${_icons}ic_arrow_down.svg';
+  static const icArrowUp = '${_icons}ic_arrow_up.svg';
+  static const icArrowRight ='${_icons}ic_arrow_right.svg';
   static const String iconCancel = "${_icons}ic_cancel.svg";
   static const String iconError = "${_icons}ic_error.svg";
   static const String iconSuccess = "${_icons}ic_success.svg";
+  static const String iconSearch = "${_icons}ic_search.svg";
+  static const String iconCalender = "${_icons}ic_calendar.svg";
+  static const String iconMoney = "${_icons}ic_money.svg";
+
 
 }
