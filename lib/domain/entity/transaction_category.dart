7e6a5d0384c12193d8a95a263cc8d817fed399be
Zyad <zyadhammad531@gmail.com>
2026-01-17 02:31:12 +0200

    feat: add domain for transactions (entities and repository)

---
+++ lib/domain/entity/transaction_category.dart
@@ -0,0 +1,10 @@
+class TransactionCategory {
+  final int id;
+  final String name;
+
+  TransactionCategory({required this.id, required this.name});
+
+  TransactionCategory copyWith({int? id, String? name}) {
+    return TransactionCategory(id: id ?? this.id, name: name ?? this.name);
+  }
+}
