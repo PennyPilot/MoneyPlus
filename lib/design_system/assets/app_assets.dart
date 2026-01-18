b9fac5dab38da602ed47a0d3a74d8a93df72189a
mohamedshemees <mohamedessampd@gmail.com>
2026-01-19 01:31:53 +0200

    feat: add forget password screen assets

--- lib/design_system/assets/app_assets.dart
+++ lib/design_system/assets/app_assets.dart
@@ -1,13 +1,26 @@
 class AppAssets {
   AppAssets._();
 
-  static const _icons = 'assets/icons';
+  static const _icons = 'assets/icons/';
 
-  static const icArrowDown = '$_icons/ic_arrow_down.svg';
-  static const icArrowUp = '$_icons/ic_arrow_up.svg';
-  static const icArrowRight ='$_icons/ic_arrow_right.svg';
+  static const icArrowDown = '${_icons}ic_arrow_down.svg';
+  static const icArrowUp = '${_icons}ic_arrow_up.svg';
+  static const icArrowRight = '${_icons}ic_arrow_right.svg';
   static const String iconCancel = "${_icons}ic_cancel.svg";
   static const String iconError = "${_icons}ic_error.svg";
   static const String iconSuccess = "${_icons}ic_success.svg";
-
+  static const String imgForgetPasswordLock = "${_icons}img_forget_password_lock.png";
+  static const String icEmail = "${_icons}ic_email.svg";
+  static const String icAppBrand = "${_icons}ic_app_brand.svg";
+  static const String icHomeGray = "${_icons}ic_home_gray.svg";
+  static const String icArrowLeft = "${_icons}ic_arrow_left.svg";
+  static const String icAccountGray = "${_icons}ic_account_gray.svg";
+  static const String icHomePrimary = "${_icons}ic_home_primary.svg";
+  static const String icNotification = "${_icons}ic_notification.svg";
+  static const String icAccountPrimary = "${_icons}ic_account_primary.svg";
+  static const String icStatisticsGray = "${_icons}ic_statistics_gray.svg";
+  static const String icTransactionGray = "${_icons}ic_transaction_gray.svg";
+  static const String icNormalArrowDown = "${_icons}ic_normal_arrow_down.svg";
+  static const String icStatisticsPrimary = "${_icons}ic_statistics_primary.svg";
+  static const String icTransactionPrimary = "${_icons}ic_transaction_primary.svg";
 }
