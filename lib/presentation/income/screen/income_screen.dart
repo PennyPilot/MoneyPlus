51c02ae1bc6615f1773f949e9d222575aa9e81db
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2026-01-19 20:58:21 +0300

    feat: add submit button to income screen

--- lib/presentation/income/screen/income_screen.dart
+++ lib/presentation/income/screen/income_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:moneyplus/design_system/assets/app_assets.dart';
 
 import '../../../core/l10n/app_localizations.dart';
+import '../../../design_system/component/buttons/button/default_button.dart';
 import '../../../design_system/theme/money_extension_context.dart';
 import '../../../design_system/widgets/app_bar.dart';
 import '../../../design_system/widgets/text_field.dart';
@@ -79,6 +80,15 @@ class IncomeScreen extends StatelessWidget {
               maxLines: 5,
               onChanged: (value) {},
             ),
+            Spacer(),
+            Padding(
+              padding: EdgeInsets.only(bottom: 19),
+              child: DefaultButton(
+                text: l10n.add,
+                onPressed: (){},
+                isEnabled: false,
+              ),
+            )
           ],
         ),
       ),
