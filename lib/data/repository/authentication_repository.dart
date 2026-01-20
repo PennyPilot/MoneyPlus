f24a35479c8decdbb1e7eed1e84298e0f6bbd2d1
Aboud <gh.abd.com.207@gmail.com>
2026-01-20 10:41:45 -0800

    feat: implement AuthenticationRepository

--- lib/data/repository/authentication_repository.dart
+++ lib/data/repository/authentication_repository.dart
@@ -1,5 +1,18 @@
+import 'package:moneyplus/data/service/supabase_service.dart';
+import 'package:moneyplus/domain/entity/user.dart' as user_entity;
 import '../../domain/repository/authentication_repository.dart';
 
-class AuthenticationRepositoryImp implements AuthenticationRepository {
+class AuthenticationRepositoryImpl implements AuthenticationRepository {
+  final SupabaseService _supabaseService;
+  AuthenticationRepositoryImpl(this._supabaseService);
 
+  @override
+  Future<void> register(user_entity.User user, String password) async {
+    final client = await _supabaseService.getClient();
+    await client.auth.signUp(
+      email: user.email,
+      password: password,
+      data: { "name" : user.username}
+    );
+  }
 }
