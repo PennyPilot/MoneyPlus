1fd3d6dabe761738b2685358782dce25d92b2934
Aboud <gh.abd.com.207@gmail.com>
2026-01-20 10:43:40 -0800

    feat: add create account dependencies

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -4,6 +4,8 @@ import '../data/repository/authentication_repository.dart';
 import '../data/service/app_secrets_provider.dart';
 import '../data/service/supabase_service.dart';
 import '../domain/repository/authentication_repository.dart';
+import '../domain/validator/authentication_validator.dart';
+import '../presentation/createAccount/cubit/create_account_cubit.dart';
 import '../presentation/home/cubit/home_cubit.dart';
 import '../presentation/login/cubit/login_cubit.dart';
 
@@ -15,9 +17,18 @@ void initDI() {
     () => SupabaseService(appSecretsProvider: getIt<AppSecretsProvider>()),
   );
   getIt.registerLazySingleton<AuthenticationRepository>(
-    () => AuthenticationRepositoryImp(),
+    () => AuthenticationRepositoryImpl(getIt<SupabaseService>()),
   );
 
   getIt.registerFactory<LoginCubit>(() => LoginCubit());
   getIt.registerFactory<HomeCubit>(() => HomeCubit());
+  getIt.registerFactory<AuthenticationValidator>(
+    () => AuthenticationValidator(),
+  );
+  getIt.registerFactory<CreateAccountCubit>(
+    () => CreateAccountCubit(
+      getIt<AuthenticationValidator>(),
+      getIt<AuthenticationRepository>(),
+    ),
+  );
 }
