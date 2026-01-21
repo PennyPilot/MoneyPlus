52c18cea6f9aadfcfa1a7d8038832db5dd301127
marah222 <maraharam222@gmail.com>
2026-01-21 20:01:48 +0300

    refactor: remove unused transaction dependencies

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,7 +1,6 @@
 import 'package:get_it/get_it.dart';
 
 import '../data/repository/authentication_repository.dart';
-import '../data/repository/transaction_repository_impl.dart';
 import '../data/service/app_secrets_provider.dart';
 import '../data/service/supabase_service.dart';
 import '../domain/repository/authentication_repository.dart';
@@ -21,13 +20,7 @@ void initDI() {
   getIt.registerLazySingleton<AuthenticationRepository>(
     () => AuthenticationRepositoryImp(),
   );
-  getIt.registerLazySingleton<TransactionRepository>(
-    () => TransactionRepositoryImpl(),
-  );
 
   getIt.registerFactory<LoginCubit>(() => LoginCubit());
   getIt.registerFactory<HomeCubit>(() => HomeCubit());
-  getIt.registerFactory<AddIncomeCubit>(
-    () => AddIncomeCubit(repository: getIt<TransactionRepository>()),
-  );
 }
