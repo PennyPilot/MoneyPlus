e5e37a1bef0205ec736bb7695e9f3c2beaaad642
Radwa85 <radwa.mohamed5033@gmail.com>
2026-01-21 04:01:27 +0200

    add amount icons to AppAssets

--- lib/design_system/assets/app_assets.dart
+++ lib/design_system/assets/app_assets.dart
@@ -5,7 +5,7 @@ class AppAssets {
 
   static const icArrowDown = '$_icons/ic_arrow_down.svg';
   static const icArrowUp = '$_icons/ic_arrow_up.svg';
-  static const icArrowRight ='$_icons/ic_arrow_right.svg';
+  static const icArrowRight = '$_icons/ic_arrow_right.svg';
   static const String iconCancel = "${_icons}ic_cancel.svg";
   static const String iconError = "${_icons}ic_error.svg";
   static const String iconSuccess = "${_icons}ic_success.svg";
@@ -17,7 +17,8 @@ class AppAssets {
   static const String icAccountGray = '$_icons/ic_account_gray.svg';
   static const String icStatisticsPrimary = '$_icons/ic_statistics_primary.svg';
   static const String icStatisticsGray = '$_icons/ic_statistics_gray.svg';
-  static const String icTransactionPrimary = '$_icons/ic_transaction_primary.svg';
+  static const String icTransactionPrimary =
+      '$_icons/ic_transaction_primary.svg';
   static const String icTransactionGray = '$_icons/ic_transaction_gray.svg';
   static const String icArrowDownRound = '$_icons/ic_arrow_down_round.svg';
   static const String icCalender = '$_icons/ic_calendar.svg';
@@ -29,5 +30,6 @@ class AppAssets {
   static const String openEye = '$_icons/ic_open_eye.svg';
   static const String tradeUp = '$_icons/ic_trade_up.svg';
   static const String tradeDown = '$_icons/ic_trade_down.svg';
-
+  static const String icAmountPrimary = '$_icons/ic_amount_primary.svg';
+  static const String icAmountGray = '$_icons/ic_amount_gray.svg';
 }
