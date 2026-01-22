a12f24ca095379fb6bbf0932985521be4080f5a6
yousef_osama11 <oyousef709@gmail.com>
2026-01-22 15:17:50 +0200

    feat: add routes for onboarding, login and home screens

---
+++ lib/presentation/navigation/routes.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:moneyplus/presentation/home/screen/home_screen.dart';
+
+part 'routes.g.dart';
+
+@TypedGoRoute<OnBoardingRoute>(path: '/')
+@immutable
+class OnBoardingRoute extends GoRouteData with $OnBoardingRoute {
+  const OnBoardingRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("onBoarding screen"),
+            ElevatedButton(onPressed: (){ LoginRoute().push(context);}, child: Text("Go to Login"))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+@TypedGoRoute<LoginRoute>(path: '/login')
+@immutable
+class LoginRoute extends GoRouteData with $LoginRoute {
+  const LoginRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Login screen"),
+            ElevatedButton(onPressed: (){ HomeRoute().push(context);}, child: Text("Go to Home"))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+@TypedGoRoute<HomeRoute>(path: '/home')
+@immutable
+class HomeRoute extends GoRouteData with $HomeRoute {
+  const HomeRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return HomeScreen();
+  }
+}
