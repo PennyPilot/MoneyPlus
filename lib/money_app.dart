1529d3e3245df1a332c4ca62b5e3a9098cb9f59e
yousef_osama11 <oyousef709@gmail.com>
2026-01-22 15:17:03 +0200

    feat: update entry point to use the router

--- lib/money_app.dart
+++ lib/money_app.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:moneyplus/design_system/theme/money_extension_context.dart';
 import 'package:moneyplus/design_system/theme/money_theme.dart';
+import 'package:moneyplus/presentation/navigation/routes.dart';
+
+
+final _router = GoRouter(
+  routes: $appRoutes,
+);
 
 class MoneyApp extends StatelessWidget {
   const MoneyApp({super.key});
@@ -8,11 +15,11 @@ class MoneyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Money++',
       theme: MoneyTheme.lightTheme,
-      home: HomeScreen(),
+      routerConfig: _router,
     );
   }
 }
