f6270addd76a2a6c2a07ab84e56f2e18c7072642
Aboud <gh.abd.com.207@gmail.com>
2026-01-25 11:19:14 -0800

    feat: add is_complete field to user sign up data

--- lib/data/repository/authentication_repository.dart
+++ lib/data/repository/authentication_repository.dart
@@ -1,9 +1,11 @@
 import 'package:moneyplus/data/service/supabase_service.dart';
 import 'package:moneyplus/domain/entity/user.dart' as user_entity;
+
 import '../../domain/repository/authentication_repository.dart';
 
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
   final SupabaseService _supabaseService;
+
   AuthenticationRepositoryImpl(this._supabaseService);
 
   @override
@@ -12,7 +14,7 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
     await client.auth.signUp(
       email: user.email,
       password: password,
-      data: { "name" : user.username}
+      data: {"name": user.username, "is_complete": false},
     );
   }
 }
