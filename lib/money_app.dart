f8df1d31b2dc92243cd172c02edfe09ea399755f
Yousef Osama Kamal <143261536+yousef-osama11@users.noreply.github.com>
2026-01-25 22:43:24 +0200

    Feature/navigation (#48)
    * feat: add go router dependency
    
    * refactor: hide BottomSheet import from material package
    
    * feat: update entry point to use the router
    
    * feat: add routes for onboarding, login and home screens
    
    * feat: add routes for onboarding, login and home screens
    
    * pull develop

--- lib/money_app.dart
+++ lib/money_app.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:go_router/go_router.dart';
 import 'package:moneyplus/design_system/theme/money_theme.dart';
-import 'package:moneyplus/presentation/home/screen/home_screen.dart';
+import 'package:moneyplus/presentation/navigation/routes.dart';
 
 import 'core/l10n/app_localizations.dart';
+
+final _router = GoRouter(
+  routes: $appRoutes,
+);
+
 
 class MoneyApp extends StatelessWidget {
   const MoneyApp({super.key});
@@ -11,7 +17,7 @@ class MoneyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Money++',
       localizationsDelegates: const [
@@ -25,7 +31,7 @@ class MoneyApp extends StatelessWidget {
         Locale('ar'),
       ],
       theme: MoneyTheme.lightTheme,
-      home: HomeScreen(),
+      routerConfig: _router,
     );
   }
 }
