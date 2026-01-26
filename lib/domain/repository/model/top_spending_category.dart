38fcb5886d1e02f35593e3876bf730329ada0119
yousef_osama11 <oyousef709@gmail.com>
2026-01-26 22:02:10 +0200

    refactor: change Float and Double types to double in TopSpendingCategory

--- lib/domain/repository/model/top_spending_category.dart
+++ lib/domain/repository/model/top_spending_category.dart
@@ -5,9 +5,9 @@ import 'package:moneyplus/domain/entity/transaction_category.dart';
 class TopSpendingCategory {
   final TransactionCategory category;
   final int numberOfTransactions;
-  final Double total;
+  final double total;
   final String currency;
-  final Float percentage;
+  final double percentage;
 
   TopSpendingCategory({
     required this.category,
