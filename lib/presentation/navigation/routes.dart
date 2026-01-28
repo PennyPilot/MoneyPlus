1eb581fc31c226aa73a11a51976559e77b9ca2a3
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2026-01-28 22:46:57 +0300

    fix build missing reference

--- lib/presentation/navigation/routes.dart
+++ lib/presentation/navigation/routes.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:moneyplus/presentation/home/screen/home_screen.dart';
 import 'package:moneyplus/presentation/login/screen/login_screen.dart';
+
+import '../../di/injection.dart';
+import '../login/cubit/login_cubit.dart';
 
 part 'routes.g.dart';
 
@@ -34,7 +38,10 @@ class LoginRoute extends GoRouteData with $LoginRoute {
 
   @override
   Widget build(BuildContext context, GoRouterState state) {
-    return LoginScreen();
+    return BlocProvider(
+      create: (context) => getIt<LoginCubit>(),
+      child: const LoginScreen(),
+    );
   }
 }
 
