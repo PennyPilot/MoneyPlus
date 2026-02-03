49680b8bd26434089cf4a6da5f01405bc6464217
yousef_osama11 <oyousef709@gmail.com>
2026-02-03 17:18:14 +0200

    feat: update login screen navigation to use HomeRoute

--- lib/presentation/login/screen/login_screen.dart
+++ lib/presentation/login/screen/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:moneyplus/design_system/assets/app_assets.dart';
 import 'package:moneyplus/design_system/theme/money_colors.dart';
 import 'package:moneyplus/design_system/theme/money_typography.dart';
 import 'package:moneyplus/design_system/widgets/buttons/money_button.dart';
+import 'package:moneyplus/presentation/navigation/routes.dart';
 import '../../../core/l10n/app_localizations.dart';
 import '../../../design_system/theme/money_extension_context.dart';
 import '../../../design_system/widgets/snack_bar.dart';
@@ -112,14 +113,7 @@ class LoginScreen extends StatelessWidget {
         title: localizations.success,
       ).showSnackBar(context: context);
 
-      // We should remove this navigator when handle the Navigator class
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) =>
-              const Scaffold(body: Center(child: Text('Welcome'))),
-        ),
-      );
+      HomeRoute().push(context);
     }
   }
 }
