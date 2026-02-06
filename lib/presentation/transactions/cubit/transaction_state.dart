768dabe30178126f14b0032d9de94f1956cbf2ff
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2026-02-06 12:24:28 +0200

    feat: add selected tab in transaction_state

--- lib/presentation/transactions/cubit/transaction_state.dart
+++ lib/presentation/transactions/cubit/transaction_state.dart
@@ -3,17 +3,20 @@ import 'package:moneyplus/core/errors/error_model.dart';
 import 'package:moneyplus/domain/entity/transaction.dart';
 
 enum TransactionStatus { initial, loading, success, failure }
+enum TransactionTabs { all, incomes, expenses }
 
 @immutable
 class TransactionState {
   final TransactionStatus status;
   final ErrorModel? error;
   final List<Transaction> transactions;
+  final TransactionTabs selectedTab;
 
   const TransactionState({
     required this.status,
     this.error,
     this.transactions = const [],
+    this.selectedTab = TransactionTabs.all
   });
 
   factory TransactionState.initial() =>
@@ -23,11 +26,13 @@ class TransactionState {
     TransactionStatus? status,
     ErrorModel? error,
     List<Transaction>? transactions,
+    TransactionTabs? selectedTab
   }) {
     return TransactionState(
       status: status ?? this.status,
       error: error,
       transactions: transactions ?? this.transactions,
+      selectedTab: selectedTab ?? this.selectedTab
     );
   }
 }
