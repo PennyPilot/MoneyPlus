084ffec3e679f0f7d76e653e8e3e8ed8010e6a86
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2026-02-07 14:54:07 +0200

    feat: add filtering behavour on transactions by selected date

--- lib/presentation/transactions/cubit/transaction_cubit.dart
+++ lib/presentation/transactions/cubit/transaction_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moneyplus/domain/entity/transaction.dart';
 import 'package:moneyplus/domain/entity/transaction_type.dart';
 import 'package:moneyplus/domain/repository/transaction_repository.dart';
 import 'package:moneyplus/presentation/transactions/cubit/transaction_state.dart';
@@ -13,8 +14,18 @@ class TransactionCubit extends Cubit<TransactionState> {
     emit(state.copyWith(status: TransactionStatus.loading));
 
     final result = await transactionRepository.getAllTransactions();
+    final now = DateTime.now();
     emit(
-      state.copyWith(transactions: result, status: TransactionStatus.success),
+      state.copyWith(
+        transactions: _filterTransaction(
+          Month.values[now.month - 1],
+          now.year,
+          result,
+        ),
+        selectedYear: now.year,
+        selectedMonth: Month.values[now.month - 1],
+        status: TransactionStatus.success,
+      ),
     );
   }
 
@@ -23,22 +34,60 @@ class TransactionCubit extends Cubit<TransactionState> {
 
     emit(state.copyWith(status: TransactionStatus.loading, selectedTab: tab));
 
-    final result = await switch (tab) {
+    final result = await _getTransactionsByTab(tab);
+
+    emit(
+      state.copyWith(
+        status: TransactionStatus.success,
+        transactions: _filterTransaction(
+          state.selectedMonth,
+          state.selectedYear,
+          result,
+        ),
+      ),
+    );
+  }
+
+  void setSelectedDate(Month month, int year) async {
+    if (state.selectedMonth == month && state.selectedYear == year) return;
+
+    emit(
+      state.copyWith(
+        selectedMonth: month,
+        selectedYear: year,
+        status: TransactionStatus.loading,
+      ),
+    );
+
+    final result = await _getTransactionsByTab(state.selectedTab);
+
+    emit(
+      state.copyWith(
+        status: TransactionStatus.success,
+        transactions: _filterTransaction(month, year, result),
+      ),
+    );
+  }
+
+  Future<List<Transaction>> _getTransactionsByTab(TransactionTabs tab) async {
+    return await switch (tab) {
       TransactionTabs.all => transactionRepository.getAllTransactions(),
       TransactionTabs.incomes => transactionRepository.getAllTransactionsByType(
         TransactionType.income,
       ),
-
       TransactionTabs.expenses =>
         transactionRepository.getAllTransactionsByType(TransactionType.expense),
     };
-
-    emit(
-      state.copyWith(status: TransactionStatus.success, transactions: result),
-    );
   }
 
-  void setSelectedDate(Month month, int year){
-    emit(state.copyWith(selectedMonth: month, selectedYear: year));
+  List<Transaction> _filterTransaction(
+    Month month,
+    int year,
+    List<Transaction> transactions,
+  ) {
+    return transactions.where((transaction) {
+      return transaction.date.year == year &&
+          transaction.date.month == month.index + 1;
+    }).toList();
   }
 }
