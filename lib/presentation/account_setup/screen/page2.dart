4145bf6639c83ec124e60796e866df66f646cfb6
nada <nadamansoursamir@gmail.com>
2026-02-09 22:57:30 +0200

    feat: Implement current balance in step 2 in account setup

--- lib/presentation/account_setup/screen/page2.dart
+++ lib/presentation/account_setup/screen/page2.dart
@@ -1,39 +1,58 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+
 import '../../../core/l10n/app_localizations.dart';
 import '../../../design_system/assets/app_assets.dart';
 import '../../../design_system/theme/money_extension_context.dart';
 import '../../../design_system/widgets/text_field.dart';
+import '../cubit/account_setup_cubit.dart';
 
 class Page2 extends StatelessWidget {
   final String currency;
-  const Page2({super.key, required this.currency});
+  final String currentBalanceState;
+
+  const Page2({super.key, required this.currency, required this.currentBalanceState});
 
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
-    final TextEditingController controller = TextEditingController();
 
     return Column(
       children: [
-        Text(l10n.how_much_money,style: context.typography.body.small.copyWith(
-          color: context.colors.body,),),
-        SizedBox(height: 24,),
+        Text(
+          l10n.how_much_money,
+          style: context.typography.body.small.copyWith(
+            color: context.colors.body,
+          ),
+        ),
+        SizedBox(height: 24),
         MTextField(
-          hint: l10n.currency,
+          hint: l10n.current_balance,
           leading: Padding(
-            padding: const EdgeInsetsDirectional.only(top: 14,bottom: 14,end: 8),
+            padding: const EdgeInsetsDirectional.only(
+              top: 14,
+              bottom: 14,
+              end: 8,
+            ),
             child: SvgPicture.asset(AppAssets.iconMoney),
           ),
-          trailing:  Padding(
-            padding: const EdgeInsetsDirectional.only(top: 14,bottom: 14,end: 8),
+          trailing: Padding(
+            padding: const EdgeInsetsDirectional.only(
+              top: 14,
+              bottom: 14,
+              end: 8,
+            ),
             child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(100),
                 color: context.colors.surface,
               ),
               child: Padding(
-                padding: const EdgeInsetsDirectional.symmetric(vertical: 4,horizontal: 8),
+                padding: const EdgeInsetsDirectional.symmetric(
+                  vertical: 4,
+                  horizontal: 8,
+                ),
                 child: Text(
                   currency,
                   style: context.typography.label.small.copyWith(
@@ -44,9 +63,9 @@ class Page2 extends StatelessWidget {
             ),
           ),
           keyboardType: TextInputType.number,
-          value: controller.text,
+          value: currentBalanceState,
           onChanged: (value) {
-            controller.text = value;
+            context.read<AccountSetupCubit>().onCurrentBalanceChanged(value);
           },
         ),
       ],
