cb9fd878ba8e415787a00616e4a2efd9cf0c7ed9
yousef_osama11 <oyousef709@gmail.com>
2026-02-28 17:43:34 +0200

    feat: register EditSalaryCubit in dependency injection

--- lib/core/di/cubit_di.dart
+++ lib/core/di/cubit_di.dart
@@ -6,6 +6,7 @@ import 'package:moneyplus/domain/repository/user_money_repository.dart';
 import 'package:moneyplus/domain/validator/authentication_validator.dart';
 import 'package:moneyplus/presentation/account_setup/cubit/account_setup_cubit.dart';
 import 'package:moneyplus/presentation/createAccount/cubit/create_account_cubit.dart';
+import 'package:moneyplus/presentation/edit_salary/edit_salary_cubit.dart';
 import 'package:moneyplus/presentation/home/cubit/home_cubit.dart';
 import 'package:moneyplus/presentation/income/cubit/add_income_cubit.dart';
 import 'package:moneyplus/presentation/login/cubit/login_cubit.dart';
@@ -43,6 +44,7 @@ void initCubitDI() {
       userMoneyRepository: getIt<UserMoneyRepository>(),
     ),
   );
+
   getIt.registerFactory<AddIncomeCubit>(
     () => AddIncomeCubit(
       transactionRepository: getIt<TransactionRepository>(),
@@ -71,7 +73,12 @@ void initCubitDI() {
       getIt<AuthenticationRepository>(),
     ),
   );
+
   getIt.registerFactory<AccountCubit>(
-        () => AccountCubit(getIt<AccountRepository>()),
+    () => AccountCubit(getIt<AccountRepository>()),
+  );
+
+  getIt.registerFactory<EditSalaryCubit>(
+    () => EditSalaryCubit(userMoneyRepository: getIt<UserMoneyRepository>()),
   );
 }
